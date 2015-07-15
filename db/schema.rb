7ba974a8188333c9c2c4981febe5d8faba8895c6
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-07-15 10:30:46 -0400

    Added image attribute to Product through paperclip gem

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150715135057) do
+ActiveRecord::Schema.define(version: 20150715141246) do
 
   create_table "prints", force: :cascade do |t|
     t.string   "name"
@@ -33,8 +33,12 @@ ActiveRecord::Schema.define(version: 20150715135057) do
     t.string   "name"
     t.string   "creator"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
