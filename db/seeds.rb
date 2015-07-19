81b06b444e1237e9b294541cae1a3966a58b31b8
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-07-18 23:26:27 -0400

    Created Navbar with links for signup, signin and profile. Began to style print index and show, adding review form to show.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,13 +7,7 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-Product.delete_all
-Product.create! id: 1, name: "Tentacle Madness", creator: "Nicolas Berrizbeitia", description: "Best redition of the absurd."
 
-Subproduct.delete_all
-Subproduct.create! product_id: 1, id: 1, quantity: 20, size: "Small", price: 19.99, active: true
-Subproduct.create! product_id: 1, id: 2, quantity: 30, size: "Medium", price: 19.99, active: true
-Subproduct.create! product_id: 1, id: 3, quantity: 15, size: "Large", price: 24.99, active: true
 
 OrderStatus.delete_all
 OrderStatus.create! id: 1, name: "In Progress"
