aa596514d92ed7b764dbc9a71e83ccfd9dcf3b46
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-07-20 22:14:31 -0400

    added out of stock to admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,7 +1,7 @@
 class AdminController < ApplicationController
 	before_action :authenticate_user!
   def main
-
+  	@subproducts = Subproduct.all
   	@prints = Print.all
   	unless current_user.admin?
   		redirect_to root_path
