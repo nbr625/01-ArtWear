f65bb0646821efbeeefea07923d22f21de915940
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-07-20 20:37:52 -0400

    fixed bug with average where it returned NaN instead of 0

--- app/models/print.rb
+++ app/models/print.rb
@@ -3,13 +3,16 @@ class Print < ActiveRecord::Base
 	has_many :reviews, dependent: :destroy
 	has_attached_file :image, styles: {large: "600x600>", medium: "300x300#", thumb: "100x100#"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-	before_save :update_subtotal
 
 
 	def average_review
 		rev_sum = reviews.collect { |review| review.blank? ? 0 : review.rating }.sum
-		rev_avg = rev_sum/(reviews.length).round(2)
-		return rev_avg
+		if rev_sum > 0
+			rev_avg = rev_sum/(reviews.length).round(2)
+			return rev_avg
+		else
+			return 0
+		end
 	end
 
 private
