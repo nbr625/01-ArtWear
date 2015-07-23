7b5b917e9e01804ba0f251be44b7639dd5abceab
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-07-22 22:13:55 -0400

    Styled front, print index and print show comment section

--- app/models/print.rb
+++ app/models/print.rb
@@ -1,7 +1,7 @@
 class Print < ActiveRecord::Base
 	belongs_to :user
 	has_many :reviews, dependent: :destroy
-	has_attached_file :image, styles: {large: "600x600>", medium: "300x300#", thumb: "100x100#"}
+	has_attached_file :image, styles: {large: "600x600>", medium: "400x600#", thumb: "100x100#"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	has_many :pledges, dependent: :destroy
 	has_many :users, through: :pledges
