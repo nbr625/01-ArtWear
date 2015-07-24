b546fffdaf25847e69fac7e90559adb93a82c895
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-07-24 10:34:12 -0400

    Styled front added final banners also fixed the shop show

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,5 @@
 class Product < ActiveRecord::Base
-	has_attached_file :image, styles: {large: "600x600>", medium: "150x150#", small:"200x200#", thumb: "100x100#"}
+	has_attached_file :image, styles: {large: "800x800>", medium: "150x150#", small:"200x200#", thumb: "100x100#"}
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	belongs_to :user
 	belongs_to :print
