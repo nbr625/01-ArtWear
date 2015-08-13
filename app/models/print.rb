192e3dba29d1fce3c6c0fecd1e3679037532ec8e
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-08-13 18:21:58 -0400

    added explicit s3 specifications onto the models

--- app/models/print.rb
+++ app/models/print.rb
@@ -1,7 +1,8 @@
 class Print < ActiveRecord::Base
 	belongs_to :user
 	has_many :reviews, dependent: :destroy
-	has_attached_file :image, styles: {large: "600x600>", medium: "400x600#", small: "150x150#", thumb: "100x100#"}
+	has_attached_file :image, styles: {large: "600x600>", medium: "400x600#", small: "150x150#", thumb: "100x100#"} \\
+	storage: :s3, bucket: 'artware', s3_credentials: S3_CREDENTIALS
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	has_many :pledges, dependent: :destroy
 	has_many :users, through: :pledges
