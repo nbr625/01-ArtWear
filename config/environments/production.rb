0a27f5f02588d6a5e30b63d5290f506670ae7dfe
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-08-13 16:59:41 -0400

    updated paperclip to use s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,9 +82,9 @@ Rails.application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :s3_credentials => {
-      :bucket => ENV['S3_BUCKET_NAME'],
-      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      :bucket => ENV['artware'],
+      :access_key_id => ENV['AKIAJJJJV6Y2D5T5Z6UA'],
+      :secret_access_key => ENV['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']
     }
   }
 end
