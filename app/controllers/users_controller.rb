235e728e23c2bbd374bd09e69cbc04ad129d4f6f
Nicolas Berrizbeitia <nbr625@gmail.com>
2015-08-19 20:34:36 -0400

    added styling to the profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,13 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @products = Product.all
+    @user_products = @products.where(creator: @user.username)
 
     respond_to do |format|
         format.html # show.html.erb
         format.xml { render :xml => @user }
     end
   end
+
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -8,13 +8,10 @@ class Product < ActiveRecord::Base
 	has_many :reviews, dependent: :destroy
 	has_many :products
 	has_many :subproducts, dependent: :destroy
+	
 
 
-	def update_user_name
-		user = User.find(params[username: self.creator])
-		self.user_id = self.user.id
-		self.save
-	end
+	
 
 	def s3_credentials
 	  	{:bucket => "artware", :access_key_id => "AKIAJXZAAVX5X6T3T34A", :secret_access_key => "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"}
